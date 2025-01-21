77a8e623a8cb3468eb0c7fe7134bc08f3c5905be
Ayush Kumar <137688691+aayush-916@users.noreply.github.com>
2025-01-21 21:42:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router ,Route , Routes } from 'react-router-dom';
 import Home from "./Home";
-import Nav from "./Nav";
 //npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' exact Component={Nav} />
+          <Route path='/' exact Component={Home} />
           <Route path='/home' exact Component={Home} />
         </Routes>
       </Router>
